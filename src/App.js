aa419bbef555adf5a0297fb0ec4ffd6af98bf508
Garrett Yaworski <garrett_yaworski@neimanmarcus.com>
2021-08-14 12:19:12 -0500

    finished code along

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Nav from "./Components/Nav/Nav";
+import routes from './routes';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        {routes}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Post/Post.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import posts from "./../../post_data.json";
+import "./Post.css";
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      content: ""
+    };
+  }
+
+  updatePostInfo = () => {
+    let post = posts.find(post => post.id === +this.props.match.params.id);
+    this.setState({
+      title: post.title,
+      content: post.content
+    });
+  }
+
+  componentDidMount() {
+    // This is where you would make an axios call to a server in a fullstack application
+    // but for today we'll be just be filtering an array of dummy data
+    this.updatePostInfo()
+  }
+
+  componentDidUpdate(prevProps){
+    if(prevProps.match.params.id !== this.props.match.params.id){
+      this.updatePostInfo();
+    }
+  }
+
+  render() {
+    const { title, content } = this.state;
+    return (
+      <div className="Post">
+        <h1>{title}</h1>
+        <p>{content}</p>
+      </div>
+    );
+  }
+}
+
+export default Post;
